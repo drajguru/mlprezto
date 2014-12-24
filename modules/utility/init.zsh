fe95bcaa800146a6f1a155dd3df0ac3703f94435
Maxime Loliée <m@siliadev.com>
2014-12-24 16:29:40 +0100

    docs(readme) update for my ls

--- modules/utility/init.zsh
+++ modules/utility/init.zsh
@@ -91,9 +91,8 @@ else
 fi
 
 alias l='ls -1A'         # Lists in one column, hidden files.
-alias ll='ls -lh'        # Lists human readable sizes.
+alias ll='ls -lah'        # Lists human readable sizes.
 alias lr='ll -R'         # Lists human readable sizes, recursively.
-alias la='ll -A'         # Lists human readable sizes, hidden files.
 alias lm='la | "$PAGER"' # Lists human readable sizes, hidden files through pager.
 alias lx='ll -XB'        # Lists sorted by extension (GNU only).
 alias lk='ll -Sr'        # Lists sorted by size, largest last.
